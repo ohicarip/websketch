a4c755867ecb4a9a9f0e7192991c475c3e7ddc43
Roger Tsang <zenghanzhang46@gmail.com>
2020-05-27 23:51:07 -0700

    Upload draw

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,12 +2,17 @@ class Board {
   private _canvas: HTMLCanvasElement;
   private _canvas2d: CanvasRenderingContext2D;
 
-  private _startX: number;
-  private _startY: number;
+  private _isDrawing: boolean;
+  private _canvasX: number[];
+  private _canvasY: number[];
 
   constructor() {
     this._canvas = document.getElementById("webSketchCanvas") as HTMLCanvasElement;
     this._canvas2d = this._canvas.getContext("2d");
+
+    this._isDrawing = false;
+    this._canvasX = [];
+    this._canvasY = [];
 
     this._setupDrawingBoard(this._canvas2d);
     this._setupEventHandler(this._canvas);
@@ -15,6 +20,30 @@ class Board {
 
   public start() {
     console.log("Canvas Board Loaded");
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  public render(timestamp: any) {
+    if (this._canvasX.length > 0 && this._canvasY.length > 0) {
+      for (let i = 0; i < this._canvasX.length - 1; i++) {
+        this._drawBetween(this._canvas2d,
+          this._canvasX[i], this._canvasY[i],
+          this._canvasX[i+1], this._canvasY[i+1]);
+      }
+
+      if (this._isDrawing) {
+        const lastX = this._canvasX[this._canvasX.length - 1];
+        const lastY = this._canvasY[this._canvasY.length - 1];
+
+        this._canvasX = [];
+        this._canvasY = [];
+
+        this._canvasX.push(lastX);
+        this._canvasY.push(lastY);
+      }
+    }
+
+    requestAnimationFrame(this.render.bind(this));
   }
 
   private _setupDrawingBoard(ctx2d: CanvasRenderingContext2D) {
@@ -31,8 +60,11 @@ class Board {
     ctx.addEventListener("mouseout", this.__mouseoutHandler.bind(this));
   }
 
-  private _drawBetween(ctx2d: CanvasRenderingContext2D,
-               x1: number, y1: number, x2: number, y2: number) {
+  private _drawBetween(
+    ctx2d: CanvasRenderingContext2D,
+    x1: number, y1: number,
+    x2: number, y2: number)
+  {
     ctx2d.beginPath();
     ctx2d.moveTo(x1, y1);
     ctx2d.lineTo(x2, y2);
@@ -40,31 +72,31 @@ class Board {
   }
 
   private __mouseDownHandler(e: MouseEvent) {
+    this._isDrawing = true;
     const canvasX = e.pageX - this._canvas.offsetLeft;
     const canvasY = e.pageY - this._canvas.offsetTop;
-    this._startX = canvasX;
-    this._startY = canvasY;
+    this._canvasX.push(canvasX);
+    this._canvasY.push(canvasY);
   }
 
   private __mouseMoveHandler(e: MouseEvent) {
-
+    if (this._isDrawing) {
+      const canvasX = e.pageX - this._canvas.offsetLeft;
+      const canvasY = e.pageY - this._canvas.offsetTop;
+      this._canvasX.push(canvasX);
+      this._canvasY.push(canvasY);
+    }
   }
 
   private __mouseUpHandler(e: MouseEvent) {
-    const canvasX = e.pageX - this._canvas.offsetLeft;
-    const canvasY = e.pageY - this._canvas.offsetTop;
-
-    this._drawBetween(this._canvas2d, this._startX, this._startY, canvasX, canvasY);
+    this._isDrawing = false;
   }
 
-  private __mouseoutHandler(e: MouseEvent)
-  {
-    const canvasX = e.pageX - this._canvas.offsetLeft;
-    const canvasY = e.pageY - this._canvas.offsetTop;
-
-    this._drawBetween(this._canvas2d, this._startX, this._startY, canvasX, canvasY);
+  private __mouseoutHandler(e: MouseEvent) {
+    this._isDrawing = false;
   }
 }
+
 
 function main() {
   let b = new Board();
